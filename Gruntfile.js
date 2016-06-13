768eee9015a11b3f28441ff9a9253bf6dceffd91
fabianonunes <fabiano@nunes.me>
2016-06-13 10:46:50 -0300

    permitir seleção de porta no grunt dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,28 @@
 /*jshint node:true*/
 
-'use strict';
+'use strict'
 
 module.exports = function (grunt) {
-
-    require('load-grunt-config')(grunt, {
-      data : {
-        config: {
-          app: 'app',
-          dist: 'dist',
-          fonts: ['senado.css', 'bootstrap']
-        }
+  require('load-grunt-config')(grunt, {
+    data: {
+      config: {
+        app: 'app',
+        dist: 'dist',
+        fonts: ['senado.css', 'bootstrap']
       }
-    })
+    }
+  })
 
-    grunt.registerTask('dev', '--allow-remote para permitir acesso externo', function (target) {
+  grunt.registerTask('dev', '--allow-remote para permitir acesso externo', function (target) {
+    if (grunt.option('allow-remote')) {
+      grunt.config.set('connect.options.hostname', '*')
+    }
 
-        if (grunt.option('allow-remote')) {
-            grunt.config.set('connect.options.hostname', '*')
-        }
-
-        grunt.task.run(['_dev'])
-
-    })
-
+    var port = parseInt(grunt.option('port'), 10)
+    if (port) {
+      grunt.config.set('connect.options.port', port)
+      grunt.config.set('connect.options.livereload', port + 100)
+    }
+    grunt.task.run(['_dev'])
+  })
 }
