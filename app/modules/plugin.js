18d0dd82a25a8bdb6b8126cd5c362eb028da061b
fabianonunes <fabiano@nunes.me>
2016-06-15 12:33:40 -0300

    anexar progresso ao plugin jukebox

--- app/modules/plugin.js
+++ app/modules/plugin.js
@@ -4,7 +4,7 @@ var $ = require('jquery')
 var pluginify = require('pluginify')
 
 var jukebox = require('./jukebox')
-var discr = require('./disc.js')
+var discr = require('./disc')
 
 var defaults = {
   currentTrack: 0,
@@ -18,7 +18,14 @@ var factory = function (el, opts) {
   var jkbx = jukebox($mediaElement)
   var disc = discr(opts)
 
-  jkbx.point(disc, /* quiet */ true)
+  setTimeout(function () {
+    jkbx.point(disc, /* quiet */ true)
+  }, 0)
+
+  var progress = opts.progress
+  if (progress) {
+    jkbx.on('progress', progress)
+  }
 
   return jkbx
 }
