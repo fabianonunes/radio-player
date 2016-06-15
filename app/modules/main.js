28e9a2fe3d92deae475d12dbf46b3ace7811848c
fabianonunes <fabiano@nunes.me>
2016-06-15 12:33:52 -0300

    progressbar e geminiscroll

--- app/modules/main.js
+++ app/modules/main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var $ = require('jquery')
+var progressbar = require('./progressbar')
 require('./plugin')
 
 // require('./disc-player')
@@ -10,22 +11,50 @@ var template = require('./templates/tracks.jade')
 
 var data = require('./fixtures/videos.json')
 var $video = $('#video')
-var $progress = $('#progress')
+var $toggle = $('#toggle')
+var $progress = $('.js-progress')
+var $stdout = $('.js-stdout')
+var bar = progressbar($progress)
 
 $video.jukebox({
-  tracks: data.tracks
+  tracks: data.tracks,
+  progress: function (p) {
+    bar.slide(p.progress)
+  }
 })
 
 var jukebox = $video.data('plugin-jukebox')
-
-jukebox.on('cued', function (track) {
-  console.log('cued', track.idx)
+jukebox.on('state', function (state) {
+  $stdout.html(state)
 })
 
-jukebox.on('progress', function (data) {
-  $progress.val(data.progress * 100)
+$toggle.click(jukebox.toggle)
+
+bar.on('change', function (v) {
+  jukebox.search(v.progress)
+})
+
+$progress.on('change', function () {
+  jukebox.search(this.value)
+})
+
+jukebox.on('cued', function (track) {
+  bar.pips(jukebox.disc().composition())
+  $('.js-track').removeClass('+light')
+  $('#js-track-' + track.idx).addClass('+light')
 })
 
 var $output = $('.js-output')
-// var $goto = $('#goto')
 $output.html(template({ tracks: data.tracks }))
+
+$('.js-track').click(function (evt) {
+  var idx = $(this).data().trackIdx
+  jukebox.tune(idx)
+  evt.preventDefault()
+})
+
+var Gemini = require('gemini-scrollbar')
+new Gemini({
+  element: document.querySelector('.js-gemini'),
+  autoshow: true
+}).create()
